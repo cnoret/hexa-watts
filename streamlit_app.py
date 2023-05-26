6981aae8c63da79236e454187b992bf8ed750bd9
Christophe Noret <123268925+cnoret@users.noreply.github.com>
2023-05-26 07:55:21 +0200

    Base 0.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,20 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 from PIL import Image
+
+def display_intro():
+    st.subheader("Analyse et prédiction de la consommation \
+                 électrique en France")
+    st.write("Projet réalisé par Titi, Tata, Toto et Tutu")
+    code = "print('Hello Team !')"
+    st.code(code, language = 'python')
+
 
 if __name__ == "__main__":
     st.set_page_config(
         page_title = "Consommation d'électricité en France",
         layout = "centered"
     )
+
+st.title("Titre du projet énergie à définir")
+display_intro()

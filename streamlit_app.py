02248e6096bf4dce0dc5e7bb4a35a6736108aef0
Christophe Noret <123268925+cnoret@users.noreply.github.com>
2023-05-26 07:41:04 +0200

    Base

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+import pandas as pd
+from PIL import Image
+
+if __name__ == "__main__":
+    st.set_page_config(
+        page_title = "Consommation d'électricité en France",
+        layout = "centered"
+    )

0e1901cd6fe1afaa739b4a7fe47a9f4e80018d3b
Christophe Noret <123268925+cnoret@users.noreply.github.com>
2023-05-26 08:08:56 +0200

    + Sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import pandas as pd
 from PIL import Image
 
 def display_intro():
+    st.title("Titre du projet énergie à définir")
     st.subheader("Analyse et prédiction de la consommation \
                  électrique en France")
     st.write("Projet réalisé par Titi, Tata, Toto et Tutu")
@@ -11,10 +13,16 @@ def display_intro():
 
 
 if __name__ == "__main__":
-    st.set_page_config(
-        page_title = "Consommation d'électricité en France",
-        layout = "centered"
-    )
+    st.set_page_config(page_title = "Consommation d'électricité en France")
+    with st.sidebar:
+        choice = option_menu(
+        menu_title = "Le Projet",
+        options=["Intro", 
+                 "Explo", 
+                 "Analyses et visualisations", 
+                 "Prédictions", 
+                 "Conclusion", 
+                 "Data"],
+        default_index=0)
 
-st.title("Titre du projet énergie à définir")
 display_intro()

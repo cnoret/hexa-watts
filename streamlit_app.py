86158b0214d88864ffd52a27c471426e693af6f9
Christophe Noret <c.noret@outlook.fr>
2023-06-27 01:33:32 +0200

    Early pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,61 +1,90 @@
+# Packages
+import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
 
 
-def display_intro():
-    """ Page d'introduction au projet"""
-
+def introduction():
     image_top = Image.open("images/nucleaire.jpeg")
-    st.image(image_top)
-    st.title("Energie_France")
-    st.subheader("Analyse et prédiction de la consommation \
-                 électrique en France")
-    st.write("Par Christophe NORET, Siham HOUCHI, Cyril LECAILLE et Simon BERRY")
+    st.image(image_top, caption = "Centrale nucléaire de Saint Laurent des Eaux \
+             - Photo : Sipa / Patrick SICCOLI", width = 500)
+    st.title("Projet ")
+    st.subheader("Analyse et prédiction de la consommation électrique en France")
 
-    st.subheader("But du projet")
+    st.subheader("Contexte")
     st.write("TEXT")
 
     st.subheader("Objectifs de l'analyse")
     st.write("TEXT")
 
-    st.subheader("Outils et compétences utilisées")
-    st.write("TEXT")
+def exploration():
+    st.title("Exploration des données")
+
+def preparation():
+    st.title("Préparation des données")
+
+def visualisation():
+    st.title("Analyses et visualisations")
+
+def modelisation():
+    st.title("Modélisation et prédictions")
+
+def conclusion():
+    st.title("Conclusion")
+
+def ressources():
+    st.title("Ressources")
 
 
-# Titre de la page dans l'onglet du navigateur Web
+# Page title and favicon
 st.set_page_config(page_title = "Consommation d'électricité en France",
                    page_icon = "images/favicon.png")
 
-# Création de la barre latérale
+# Sidebar menu
 with st.sidebar:
-    # Ouverture puis affichage du logo de la barre latérale
     image_side = Image.open("images/triangle.png")
     st.image(image_side)
-
-    # Header correspondant au nom du projet
     st.header("Energie_France")
-
-    # Création du menu et de ses catégories
     choice = option_menu(
-        menu_title = "Menu",
-        options = ["Introduction du projet",
+        menu_title = "Sommaire",
+        options = ["Introduction",
                    "Exploration des données",
+                   "Préparation des données",
                    "Analyses et visualisations",
-                   "Prédictions",
+                   "Modélisation et prédictions",
                    "Conclusion",
-                   "Liens et ressources"],
+                   "Ressources"],
         default_index = 0)
+    st.header("Equipe du projet :")
+    st.markdown('- Christophe NORET&nbsp;&nbsp;[<img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width=25>](http://www.linkedin.com/in/cnoret) \
+                [<img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width=25>](http://www.linkedin.com/in/cnoret)', unsafe_allow_html=True)
+    st.markdown('- Siham HOUCHI&nbsp;&nbsp;[<img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width=25>](http://www.linkedin.com/in/cnoret) \
+                [<img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width=25>](http://www.linkedin.com/in/cnoret)', unsafe_allow_html=True)
+    st.markdown('- Cyril LECAILLE&nbsp;&nbsp;[<img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width=25>](http://www.linkedin.com/in/cnoret) \
+                [<img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width=25>](http://www.linkedin.com/in/cnoret)', unsafe_allow_html=True)
+    st.markdown('- Simon BERRY&nbsp;&nbsp;[<img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width=25>](http://www.linkedin.com/in/cnoret) \
+                [<img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width=25>](http://www.linkedin.com/in/cnoret)', unsafe_allow_html=True)
 
-    # Bloc de l'équipe
-    st.write("Equipe du projet :")
-    st.subheader("- Christophe NORET")
-    st.write("LinkedIn / Github")
-    st.subheader("- Siham HOUCHI")
-    st.write("LinkedIn / Github")
-    st.subheader("- Cyril LECAILLE")
-    st.write("LinkedIn / Github")
-    st.subheader("- Simon Berry")
-    st.write("LinkedIn / Github")
 
-display_intro()
+if choice == "Introduction":
+    introduction()
+
+elif choice == "Exploration des données":
+    exploration()
+
+elif choice == "Préparation des données":
+    preparation()
+
+elif choice == "Analyses et visualisations":
+    visualisation()
+
+elif choice == "Modélisation et prédictions":
+    modelisation()
+
+elif choice == "Conclusion":
+    conclusion()
+
+else:
+    ressources()
+    

fedd5d00952fe7f1a5dede87ddb5718f4d585f54
Christophe Noret <123268925+cnoret@users.noreply.github.com>
2023-09-21 00:15:56 +0200

    PEP

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ from streamlit_option_menu import option_menu
 from PIL import Image
 
 # Importing pages
+
 from content.introduction import introduction
 from content.exploration import exploration
 from content.preparation import preparation
@@ -11,11 +12,13 @@ from content.modelisation import modelisation
 from content.conclusion import conclusion
 from content.ressources import ressources
 
-# Page title & favicon
+## Page title & favicon
+
 st.set_page_config(page_title = "Consommation d'électricité en France",
                    page_icon = "images/favicon.png")
 
-# Sidebar menu
+## Sidebar menu
+
 with st.sidebar:
     image_side = Image.open("images/triangle.png")
     st.image(image_side)
@@ -30,8 +33,8 @@ with st.sidebar:
                    "Conclusion",
                    "Ressources"],
         default_index = 0)
-    
-    ## Team
+ 
+    # Team
     st.header("Equipe du projet :")
     st.markdown('- Christophe NORET&nbsp;&nbsp;[<img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width=25>](http://www.linkedin.com/in/cnoret) \
                 [<img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width=25>](http://www.linkedin.com/in/cnoret)', unsafe_allow_html = True)

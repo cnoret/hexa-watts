f380c79f501250a84471d31f33f5052ff6750b97
Christophe Noret <123268925+cnoret@users.noreply.github.com>
2023-10-13 19:38:30 +0200

    RB

---
+++ content/eco2mix_code.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import time
+from datetime import datetime as dt
+import numpy as np
+
+# Visualisation
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+import seaborn as sns
+import ipywidgets as widgets
+from ipywidgets import interact
+from ipywidgets.widgets.widget_selection import DescriptionWidget
+import pyarrow.parquet as pq
+import pyarrow as pa
+import plotly.graph_objects as go
+import plotly.express as px
+import geopandas as gpd
+import colorcet as cc
+# %matplotlib inline Commenté car non supporté par StreamLit
+
+# Importation du dataset principal
+
+table = pq.read_table('datasets/df.parquet')
+df = table.to_pandas()
+
+# Suppression de la colonne vide
+df = df.drop(["Column 30"], axis = 1)
+
+## Notre dataset est très propre d'origine, les NaN correspondent uniquement
+## à une production/consommation nulle. Nous pourrions simplement boucler sur
+## l'intégralité du dataset mais allons procéder en découpage par étapes.
+## Les valeurs manquantes des TCO/TCH seront traités dans un focus dédié.
+
+# Traitement des NaN : Nucléaire
+df['Nucléaire (MW)'] = df['Nucléaire (MW)'].fillna(0)
+
+# Traitement des NaN : Pompage
+regions_sans_pompage = ['Centre-Val de Loire', 'Normandie', 'Nouvelle-Aquitaine',
+                      'Pays de la Loire', 'Île-de-France']
+
+# Remplacement des NaN des régions sans stations par une valeur nulle
+df.loc[df['Région'].isin(regions_sans_pompage), 'Pompage (MW)'] = \
+df.loc[df['Région'].isin(regions_sans_pompage), 'Pompage (MW)'].fillna(0)
+
+# Remplacement des NaN des HDF par une valeur nulle
+df.loc[df['Région'].isin(['Hauts-de-France']), 'Pompage (MW)'] = \
+df.loc[df['Région'].isin(['Hauts-de-France']), 'Pompage (MW)'].fillna(0)
+
+# Traitement des NaN : Distribution Nationale et Batteries
+df['Ech. physiques (MW)'] = df['Ech. physiques (MW)'].fillna(0)
+df['Stockage batterie'] = df['Stockage batterie'].fillna(0)
+df['Déstockage batterie'] = df['Déstockage batterie'].fillna(0)
+
+# Traitement des NaN : Éolien
+df['Eolien terrestre'] = df['Eolien terrestre'].fillna(0)
+df['Eolien offshore'] = df['Eolien offshore'].fillna(0)
+df['Eolien (MW)'] = df['Eolien (MW)'].fillna(0)
+
+# Ajout de colonnes temporelles
+df['Date - Heure'] = pd.to_datetime(df['Date - Heure'],
+                                    format = '%Y-%m-%d %H:%M:%S%z',
+                                    errors = 'coerce', utc = True)
+df['Jour'] = df['Date - Heure'].dt.day_name() # Ajout du jour de la semaine
+df['Jour_mois'] = df['Date - Heure'].dt.day # Jour du mois
+df['Mois'] = df['Date - Heure'].dt.month_name() # Mois de l'année
+df['Année'] = df['Date - Heure'].dt.year # Année
+
+# Création d'une colonne Week-end pour indiquer s'il s'agit d'un
+# jour de la semaine ou d'un week-end.
+week_end = ['Saturday', 'Sunday']
+df['Week-End'] = df['Jour'].apply(lambda day: 1 if day in week_end else 0)
+
+## Dans notre étude, nous souhaitons conserver que les années complètes,
+## nous supprimons donc 2022 du scope.
+df = df[(df['Année'] != 2022) & (df['Année'] > 2012)].copy()
+
+# Ajout d'une colonne qui indique la somme de production totale confondue
+df.loc[:, 'Production (MW)'] = df['Bioénergies (MW)'] + df['Eolien (MW)']  \
+                        + df['Hydraulique (MW)'] + df['Nucléaire (MW)'] \
+                        + df['Solaire (MW)'] + df['Thermique (MW)']
+
+## Pendant certaines périodes de trente minutes, certaines régions ne produisent
+## pas d'énergie, ce qui conduit à des valeurs manquantes dans les données.
+## Ces valeurs NaN sont donc remplacées par des zéros.
+df.loc[:, 'Production (MW)'] = df['Production (MW)'].fillna(0)
+
+# Ajout d'une colonne 'Cotier' pour déterminer si la région est côtière ou non
+cotes = ['Bretagne', 'Normandie','Hauts-de-France',
+       'Pays de la Loire','Nouvelle-Aquitaine','Occitanie',
+       "Provence-Alpes-Côte d'Azur"]
+
+df['Cotier'] = df['Région'].apply(lambda region: 1 if region in cotes else 0)
+
+# Ajout d'une colonne "Nordsud" pour déterminer la position d'une région
+def nordsud(reg):
+  "Fonction retournant une chaîne 'Nord' / 'Sud' suivant la région passée en argument"
+
+  nord = ['Bretagne', 'Normandie', 'Pays de la Loire', 'Centre-Val de Loire',
+          'Île-de-France', 'Hauts-de-France', 'Grand Est', 'Bourgogne-Franche-Comté']
+  sud = ['Nouvelle-Aquitaine', 'Occitanie', 'Auvergne-Rhône-Alpes',
+         "Provence-Alpes-Côte d'Azur"]
+  if reg in nord:
+    return 'Nord'
+  elif reg in sud:
+    return 'Sud'
+  else:
+    return np.nan
+
+# Utilisation de notre fonction nordsud sur la colonne Région
+df['Nordsud'] = df['Région'].apply(nordsud)
+
+print("NaN de la colonne NordSud :", df['Nordsud'].isna().sum())
+print("NaN de la colonne Cotier :", df['Cotier'].isna().sum())
+print("NaN de la colonne Week-End :", df['Week-End'].isna().sum())
+
+# Création d'un DataFrame pour l'analyse de la consommation.
+to_keep = ['Région', 'Nature', 'Date', 'Heure', 'Date - Heure',
+           'Jour','Mois', 'Jour_mois', 'Année', 'Consommation (MW)']
+consommation = df[to_keep]
+
+# Création d'un DataFrame pour la consommation par années
+yearly = consommation.groupby(['Année', 'Région'])['Consommation (MW)'].sum().reset_index()
+
+# Création d'un DataFrame pour l'analyse de la production
+to_loose = ['Consommation (MW)']
+production = df.drop(to_loose, axis = 1)
+
+## Création d'un DataFrame pour l'analyse des TCO/TCH
+# Sauvegarde de chaque colonnes contenant TCO ou TCH dans son nom
+tco_columns = [col for col in df.columns if 'TCO' in col]
+tch_columns = [col for col in df.columns if 'TCH' in col]
+base_columns = ['Date', 'Région']
+
+# On réassemble les colonnes et on construit un nouveau DataFrame TCO/TCH
+selected_columns = base_columns + tco_columns + tch_columns
+tco_tch = df[selected_columns]
+df = df.drop(columns = tco_columns + tch_columns)
--- content/visualisation.py
+++ content/visualisation.py
@@ -1,4 +1,102 @@
 import streamlit as st
+import pandas as pd
+from content.eco2mix_code import df
+import plotly.graph_objects as go
+import plotly.express as px
+
+
+
+## Color scheme qu'on va utiliser pour les grafs
+colors = {
+    'Thermique (MW)': 'red',
+    'Nucléaire (MW)': '#F7E237',
+    'Eolien (MW)': 'lightblue',
+    'Solaire (MW)': 'orange',
+    'Hydraulique (MW)': 'darkblue',
+    'Bioénergies (MW)': 'green'
+}
+
 
 def visualisation():
     st.title("Analyses et visualisations")
+
+    # Création du DataFrame 'consommation'
+    @st.cache
+    def create_consommation_dataframe(df):
+        to_keep = ['Région', 'Nature', 'Date', 'Heure', 'Date - Heure','Jour', 'Mois', 'Jour_mois', 'Année', 'Consommation (MW)']
+        consommation = df[to_keep]
+        return consommation
+    consommation = create_consommation_dataframe(df)
+
+    # Création du DataFrame 'production'
+    @st.cache
+    def create_production_dataframe(df):
+        to_loose = ['Consommation (MW)']
+        production = df.drop(to_loose, axis=1)
+        return production
+    production = create_production_dataframe(df)
+    
+    @st.cache
+    def create_pf1_chart(df):
+    # Notre première Data Viz PF1
+        # Regroupement de la production par année de toutes les énergies
+        prod = production.groupby(['Année'])[['Thermique (MW)', 'Nucléaire (MW)',
+                                            'Eolien (MW)','Solaire (MW)',
+                                            'Hydraulique (MW)', 'Bioénergies (MW)']].sum()
+
+        # Calcul de la production totale
+        prod_total = prod.sum(axis = 1)
+
+        # Calcul du pourcentage de la production totale
+        prod_percentage = prod.divide(prod_total, axis = 0) * 100
+
+        # Création d'un bar chart avec plotly pour la production secteur/année
+        trace_list = []
+        for sector in prod_percentage.columns:
+            trace = go.Bar(x = prod_percentage.index,
+                        y = prod_percentage[sector],
+                        name = sector,
+                        marker=dict(color=colors[sector]))
+            trace_list.append(trace)
+
+        # Personnalisation du graphique
+        layout = go.Layout(
+            barmode = 'stack', # Barres empilées
+            title = 'Production par secteur, empilée par année (% du total)',
+            xaxis = dict(title = 'Année'),
+            yaxis = dict(title = 'Pourcentage de la production totale'),
+        )
+
+        # Création puis affichage de la figure contenant nos 2 objets
+        fig = go.Figure(data = trace_list, layout = layout)
+        
+        return fig
+    
+    @st.cache
+    def create_pf2_chart(df):
+        # Création d'un DataFrame regroupant la production verte/année
+        green_yearly = df.groupby(['Année'])[['Eolien (MW)', 'Solaire (MW)', 'Hydraulique (MW)', 'Bioénergies (MW)']].sum()
+        
+        # Reset the index to make 'Année' a regular column
+        green_yearly = green_yearly.reset_index()
+
+        fig = px.bar(green_yearly, x='Année', y=green_yearly.columns[1:],  # Exclude 'Année' from the y-values
+            title="Évolution de la production d'énergie renouvelable par année",
+            labels={'Année': 'Année', 'value': 'Production (MW)'},
+            height=400, color_discrete_map=colors)
+
+        fig.update_layout(barmode='stack')
+
+        return fig
+   
+    st.title('1. Production de l\'énergie')
+
+    # PF1
+    pf1_chart = create_pf1_chart(production)
+    st.plotly_chart(pf1_chart)
+    st.write("Dépendance au nucléaire : Le graphique révèle que la France dépend largement de l'énergie nucléaire pour sa production d'électricité. Cette source d'énergie représente une part substantielle de la production totale, ce qui indique son importance dans le mix énergétique du pays. La France a historiquement investi massivement dans l'énergie nucléaire, ce qui lui a permis de disposer d'une source d'énergie fiable et à faible émission de carbone. Nous pourrons comparer cette stratégie au reste de l'Europe plus tard dans ce rapport.")
+
+    # PF2
+    pf2_chart = create_pf2_chart(production)
+    st.plotly_chart(pf2_chart)
+    st.write("Une tendance encourageante est l'augmentation de la part de l'énergie solaire et éolienne dans la production d'électricité au cours des dernières années. Cette croissance suggère que la France diversifie son mix énergétique en intégrant davantage d'énergies renouvelables. Les investissements dans le solaire et l'éolien reflètent une préoccupation croissante pour la réduction des émissions de gaz à effet de serre et la transition vers une production d'électricité plus propre et durable.")

eb98a1990f3fe46de15cc06f84a265e7356f81fc
Christophe Noret <123268925+cnoret@users.noreply.github.com>
2023-10-17 20:21:20 +0200

    Mise à jour des outils

--- content/ressources.py
+++ content/ressources.py
@@ -18,18 +18,22 @@ def ressources():
     st.title("Compétences et technologies")
     st.subheader('Python')
     st.markdown("""
-                Les bibliothèques Python suivantes ont été utilisées dans ce projet :
+                Les bibliothèques suivantes ont été utilisées dans ce projet :
                 - **[Pandas](https://pandas.pydata.org/)** : Utilisé pour la manipulation des données et l'analyse.
                 - **[Matplotlib](https://matplotlib.org/)** : Utilisé pour la création de visualisations statiques, animées et interactives.
                 - **[Seaborn](https://seaborn.pydata.org/)** : Basé sur Matplotlib, Seaborn fournit une interface de haut niveau pour dessiner des graphiques statistiques attrayants.
                 - **[Plotly](https://plotly.com/python/)** : Utilisé pour les visualisations interactives.
                 - **[Scikit-Learn](https://scikit-learn.org/stable/)** : Utilisé pour la modélisation des données, y compris la préparation, le prétraitement et la prédiction.
                 - **[Streamlit](https://streamlit.io/)** : Streamlit permet de transformer des scripts de données en applications web partageables.
+                - **[Pylint](https://www.pylint.org/)** : Outil d'analyse de code source aidant à maintenir un code homogène respectant les conventions et le style Python PEP 8.
                 """)
 
-    st.subheader('Déploiement')
+    st.subheader('Développement et Déploiement')
     st.markdown("""
                 Les outils suivants ont été utilisés pour le déploiement de ce projet :
+                - **[Jupyter Notebook](https://jupyter.org/)** : Un environnement de développement interactif qui favorise une programmation exploratoire et interactive.
+                - **[Visual Studio Code](https://code.visualstudio.com/)** : Un éditeur de code source extensible utilisé pour le développement de l'application.
+                - **[Git](https://git-scm.com/)** : Un système de contrôle de version distribué utilisé pour suivre les changements dans le code source durant le développement de l'application.
                 - **[Streamlit Cloud](https://streamlit.io/cloud)** : Utilisé pour le déploiement de l'application en ligne.
                 - **[GitHub](https://github.com/)** : Utilisé pour le contrôle de version et le stockage du code de l'application.
                 """)

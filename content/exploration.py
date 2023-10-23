0bd677d98af3abbf5b73af377b6163f35a9f1296
Christophe Noret <123268925+cnoret@users.noreply.github.com>
2023-10-23 12:29:13 +0200

    Spinner + Fixes

--- content/exploration.py
+++ content/exploration.py
@@ -1,3 +1,7 @@
+"""
+Création de la page "Exploration des données" de l'application Énergie_France
+"""
+
 import streamlit as st
 import pyarrow.parquet as pq
 import pandas as pd
@@ -11,38 +15,52 @@ def exploration():
     # On display cette image en haut de page
     st.image(image_url, width=200)
 
-    try:
-        table = pq.read_table('datasets/df.parquet')
-        df = table.to_pandas()
+    with st.spinner('Chargement des données...⏳'):
+        try:
+            table = pq.read_table('datasets/df.parquet')
+            df = table.to_pandas()
 
-        # Création de notre DataFrame "temp" contenant les températures
-        temp = pd.read_csv('datasets/temp.csv', sep = ';')    
+            # Création de notre DataFrame "temp" contenant les températures
+            temp = pd.read_csv('datasets/temp.csv', sep = ';')
 
-        # Suppression de la colonne vide
-        df = df.drop(["Column 30"], axis = 1)
+            # Suppression de la colonne vide
+            df = df.drop(["Column 30"], axis = 1)
 
-        # Calcul du pourcentage de valeur manquantes du dataframe
-        nan_count = df.isna().sum()
-        nan_percentage = (df.isna().mean() * 100).round(2)
-        nan_info = pd.concat([nan_count, nan_percentage], axis = 1, keys = ['NaN Count', '% of NaN'])
-        
+            # Calcul du pourcentage de valeur manquantes du dataframe
+            nan_count = df.isna().sum()
+            nan_percentage = (df.isna().mean() * 100).round(2)
+            nan_info = pd.concat([nan_count, nan_percentage],
+                                 axis = 1, keys = ['NaN Count', '% of NaN'])
 
-        st.subheader("Aperçu du jeu de données principal : Eco2Mix (RTE)")
-        st.write("Nombre de lignes:", df.shape[0])
-        st.write("Nombre de colonnes:", df.shape[1])
-        st.write("Échantillon de données:")
-        st.dataframe(df.head())
+        except pd.errors.EmptyDataError as e:
+            st.error(f"Une erreur s'est produite lors de la lecture du fichier CSV : {str(e)}")
+            return
 
-        st.subheader("Aperçu des valeurs manquantes")
-        st.write(nan_info)
+        except FileNotFoundError as e:
+            st.error(f"Le fichier spécifié est introuvable : {str(e)}")
+            return
 
-        st.subheader("Aperçu du jeu de données des températures")
-        st.write("Nombre de lignes:", temp.shape[0])
-        st.write("Nombre de colonnes:", temp.shape[1])
-        st.write("Échantillon de données:")
-        st.dataframe(temp.head())
-        st.write("Nombre de NaN :", temp.isna().sum().sum())
+        except KeyError as e:
+            st.error(f"La colonne spécifiée est introuvable dans le DataFrame : {str(e)}")
+            return
 
+        except Exception as e:
+            st.error(f"Une erreur inattendue s'est produite : {str(e)}")
+            return
 
-    except Exception as e:
-        st.error(f"Une erreur s'est produite lors de l'importation des données : {str(e)}")
+    # Display
+    st.subheader("Aperçu du jeu de données principal : Eco2Mix (RTE)")
+    st.write("Nombre de lignes:", df.shape[0])
+    st.write("Nombre de colonnes:", df.shape[1])
+    st.write("Échantillon de données:")
+    st.dataframe(df.head())
+
+    st.subheader("Aperçu des valeurs manquantes")
+    st.write(nan_info)
+
+    st.subheader("Aperçu du jeu de données des températures")
+    st.write("Nombre de lignes:", temp.shape[0])
+    st.write("Nombre de colonnes:", temp.shape[1])
+    st.write("Échantillon de données:")
+    st.dataframe(temp.head())
+    st.write("Nombre de NaN :", temp.isna().sum().sum())
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
+"""
+Streamlit Start
+"""
+
 import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
